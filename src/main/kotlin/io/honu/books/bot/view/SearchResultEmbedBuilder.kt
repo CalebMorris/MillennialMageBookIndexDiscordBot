49868ee5065804b64a5b71245aa9c1a03e792dfc
Caleb Morris <caleb.morris.g@gmail.com>
2023-08-12 20:32:06 -0700

    Added bolded on matching terms and footer turtle

--- src/main/kotlin/io/honu/books/bot/view/SearchResultEmbedBuilder.kt
+++ src/main/kotlin/io/honu/books/bot/view/SearchResultEmbedBuilder.kt
@@ -30,7 +30,8 @@ class SearchResultEmbedBuilder(
             }
             this.color = Color(0x0099FF)
             this.footer = EmbedBuilder.Footer().apply {
-                text = "fetched by your local Archivist"
+                this.text = "fetched by your local Archivist"
+                this.icon = "https://cdn.discordapp.com/emojis/1140113649801908367.webp"
             }
             this.fields = mutableListOf(
                 EmbedBuilder.Field().apply {
@@ -43,11 +44,24 @@ class SearchResultEmbedBuilder(
                 },
                 EmbedBuilder.Field().apply {
                     this.name = ""
-                    this.value = searchResult.segmentContent
+                    this.inline = false
+                },
+                EmbedBuilder.Field().apply {
+                    this.name = ""
+                    this.value = searchResult.segmentContent.boldDesiredWords(queryString.splitToSequence(" "))
+                    this.inline = false
+                },
+                EmbedBuilder.Field().apply {
+                    this.name = ""
                     this.inline = false
                 },
             )
         }
     }
+
+    private fun String.boldDesiredWords(terms: Sequence<String>): String =
+        terms.distinct().fold(this) { acc, term ->
+            acc.replace(Regex(term, RegexOption.IGNORE_CASE)) { matchResult: MatchResult -> "**${matchResult.value}**" }
+        }
 
 }
